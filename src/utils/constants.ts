79f3d33e243d3ec366b6915be1b6a2dbf956bf8f
kyzym <kyzym1@gmail.com>
2023-07-13 21:05:45 +0200

    fix: regex 07/13/2023 21:05:45

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,7 @@
 export const LETTERS_REGEXP = /\p{L}/u;
 export const NUMBERS_REGEXP = /\d/;
-export const SYMBOLS_REGEXP = /[^\p{L}\p{N}]/gu;
+export const SYMBOLS_REGEXP = /[!@#$%^&*()_+\-={};"\\|,.<>?]/;
+
 export const minSecureNumber = 8;
 export const STRENGTH_BAR = `strength-bar`;
 export const STRENGTH_BAR_RED = `strength-bar red`;
