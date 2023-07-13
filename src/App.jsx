8b3af6aa5786d34bfb65613b8a5629f96aca825a
kyzym <kyzym1@gmail.com>
2023-07-13 18:23:37 +0200

    init: new branch Thu, Jul 13, 2023  6:23:37 PM

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import './app.css';
+// import { useState } from 'react';
+// import './app.css';
 
 function App() {
   const [password, setPassword] = useState('');
